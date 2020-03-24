58f919410f4a1390a4b98695125654dea8f754a9
inhobaek-wj <inhobaek.wj@gmail.com>
2020-03-24 20:48:11 +0900

    use toStringAsFixed for fixed decimal place

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -32,7 +32,7 @@ class TransactionList extends StatelessWidget {
                     ),
                     padding: EdgeInsets.all(10),
                     child: Text(
-                      '\$${transactions[index].amount}', // string interpolation systax.
+                      '\$${transactions[index].amount.toStringAsFixed(2)}', // string interpolation systax.
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
                         fontSize: 20,
