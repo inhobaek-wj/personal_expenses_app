6a42d380001da01f60011038037de251f2c43da7
inhobaek-wj <inhobaek.wj@gmail.com>
2020-03-25 18:18:46 +0900

    add chart_bar.dart file as widget

---
+++ lib/widgets/chart_bar.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+  final String label;
+  final double spendingAmount;
+  final double spendingPctOfTotal;
+
+  const ChartBar(this.label, this.spendingAmount, this.spendingPctOfTotal);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+
+      ],
+    );
+  }
+}
