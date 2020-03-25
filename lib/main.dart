c7a59548357e07139268ed8b742779f4c8ebac4c
inhobaek-wj <inhobaek.wj@gmail.com>
2020-03-25 16:55:01 +0900

    use textTheme

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,24 @@ class MyApp extends StatelessWidget {
       title: 'Flutter App',
       theme: ThemeData(
         primarySwatch: Colors.purple,
-        accentColor: Colors.amber
+        accentColor: Colors.amber,
+        fontFamily: 'Quicksand',
+        appBarTheme: AppBarTheme(
+          textTheme: ThemeData.light().textTheme.copyWith(
+            headline6: TextStyle(
+              fontFamily: 'OpenSans',
+              fontSize: 20,
+              fontWeight: FontWeight.bold
+            )
+          )
+        ),
+        textTheme: ThemeData.light().textTheme.copyWith(
+          title: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 18,
+            fontWeight: FontWeight.bold
+          )
+        )
       ),
       home: MyHomePage(),
     );
