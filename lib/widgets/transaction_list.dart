7f38271738b2e86ff4b3c00a6907135f28b64019
inhobaek-wj <inhobaek.wj@gmail.com>
2020-03-25 16:31:14 +0900

    apply Theme with ThemeData

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -17,54 +17,55 @@ class TransactionList extends StatelessWidget {
         itemCount: transactions.length,
         itemBuilder: (ctx, index) {
           return Card(
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.symmetric(
-                      vertical: 10,
-                      horizontal: 15
-                    ),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.purple,
-                        width: 2
-                      )
-                    ),
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      '\$${transactions[index].amount.toStringAsFixed(2)}', // string interpolation systax.
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.purple
-                      ),
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 15
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Theme.of(context).primaryColor,
+                      width: 2
                     )
                   ),
-
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        transactions[index].title,
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold
-                        )
-                      ),
-                      Text(
-                        DateFormat('yyyy/MM/dd').format(transactions[index].date),
-                        style: TextStyle(
-                          color: Colors.grey
-                        )
-                      )
-                    ],
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    // string interpolation systax.
+                    '\$${transactions[index].amount.toStringAsFixed(2)}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Theme.of(context).primaryColor
+                    ),
                   )
-                ],
-              ),
-            );
+                ),
+
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      transactions[index].title,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold
+                      )
+                    ),
+                    Text(
+                      DateFormat('yyyy/MM/dd').format(transactions[index].date),
+                      style: TextStyle(
+                        color: Colors.grey
+                      )
+                    )
+                  ],
+                )
+              ],
+            ),
+          );
         },
         // children: transactions.map((tx) {
-        //     return 
+        //     return
         // }).toList(),
       ),
     );
