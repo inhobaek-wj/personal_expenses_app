087546a5832c9d7ada390b10f5cdaa6c63e80baa
inhobaek-wj <inhobaek.wj@gmail.com>
2020-03-25 17:49:59 +0900

    set data to draw chart

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,7 +1,33 @@
-
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../models/transaction.dart';
 
 class Chart extends StatelessWidget {
+
+  final List<Transaction> recentTransactions;
+
+  const Chart(this.recentTransactions);
+
+  List<Map<String,Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+        final weekDay = DateTime.now().subtract(Duration(days: index));
+        double totalSum = 0.0;
+
+        for (var tx in recentTransactions) {
+          if (tx.date.day == weekDay.day
+            && tx.date.month == weekDay.month
+            && tx.date.year == weekDay.year) {
+            totalSum += tx.amount;
+          }
+        }
+
+        return {
+          'day': DateFormat.E(weekDay),
+          'amount': totalSum
+        };
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +35,7 @@ class Chart extends StatelessWidget {
       elevation: 6,
       margin: EdgeInsets.all(20),
       child: Row(
-        
+
       ),
     );
   }
