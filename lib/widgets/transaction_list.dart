5f5eeea344b9f94a9ad2d1de136a496484334132
inhobaek-wj <inhobaek.wj@gmail.com>
2020-03-28 19:50:49 +0900

    refactoring: split transaction item

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
+import 'transaction_item.dart';
 import '../models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
@@ -41,45 +41,9 @@ class TransactionList extends StatelessWidget {
       : ListView.builder(
         itemCount: transactions.length,
         itemBuilder: (ctx, index) {
-          return Card(
-            margin: EdgeInsets.symmetric(
-              vertical: 8,
-              horizontal: 5
-            ),
-            elevation: 5,
-            child: ListTile(
-              leading: CircleAvatar(
-                radius: 30,
-                child: Padding(
-                  padding: EdgeInsets.all(6),
-                  child: FittedBox(
-                    child: Text('\$${transactions[index].amount}'),
-                  ),
-                )
-              ),
-
-              title: Text(
-                transactions[index].title,
-                style: Theme.of(context).textTheme.headline6
-              ),
-
-              subtitle: Text(
-                DateFormat.yMMMd().format(transactions[index].date),
-              ),
-
-              trailing: mediaQuery.size.width > 460 ?
-              FlatButton.icon(
-                label: Text('Delete'),
-                icon: Icon(Icons.delete),
-                textColor: Theme.of(context).errorColor,
-                onPressed: () => deleteTx(transactions[index].id),
-              )
-              : IconButton(
-                icon: Icon(Icons.delete),
-                color: Theme.of(context).errorColor,
-                onPressed: () => deleteTx(transactions[index].id),
-              ),
-            ),
+          return TransactionItem(
+            transaction: transactions[index],
+            deleteTx: deleteTx
           );
         },
         // children: transactions.map((tx) {
@@ -90,3 +54,5 @@ class TransactionList extends StatelessWidget {
   }
 
 }
+
+
